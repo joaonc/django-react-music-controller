6807b1825a8485ac612f426d0b1d0c3b12bc563d
Joao Coelho <joaonc@users.noreply.github.com>
2022-12-11 14:08:04 -0600

    Room model (#4)

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework import generics
+
+from .models import Room
+from .serializers import RoomSerializer
 
 
-def main(request):
-    return HttpResponse('Hello')
+class RoomViewList(generics.ListAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
+
+
+class RoomViewCreate(generics.CreateAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
