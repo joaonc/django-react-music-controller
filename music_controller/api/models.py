b29e6bd5f8ce4e902dc231a1653765588e49cb20
Joao Coelho <joaonc@hotmail.com>
2022-12-11 13:46:15 -0600

    room model

--- music_controller/api/models.py
+++ music_controller/api/models.py
@@ -1,3 +1,11 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class Room(models.Model):
+    code = models.UUIDField(default=uuid.uuid4, editable=False)
+    host = models.CharField(max_length=50, unique=True)
+    guest_can_pause = models.BooleanField(default=False, null=False)
+    votes_to_skip = models.IntegerField(default=1, null=False)
+    create_at = models.DateTimeField(auto_now_add=True)
