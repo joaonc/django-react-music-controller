7cab36e86036fa3465053fe3e8aa6276af5808d4
Joao Coelho <joaonc@users.noreply.github.com>
2022-12-11 23:15:12 -0600

    Frontend integration (#6)

--- music_controller/music_controller/urls.py
+++ music_controller/music_controller/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
+    path('', include('frontend.urls')),
 ]
