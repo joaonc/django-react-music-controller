ee838bfa981188d1060d79215cbd4a3781206a08
Joao Coelho <joaonc@users.noreply.github.com>
2022-12-12 14:38:04 -0600

    Room model serialization (#9)

--- music_controller/api/urls.py
+++ music_controller/api/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 
 from . import views
 
-
 urlpatterns = [
-    path('room/', views.RoomViewList.as_view()),
-    path('room/', views.RoomViewCreate.as_view()),
+    path('room/', views.CreateRoomView.as_view()),
 ]
