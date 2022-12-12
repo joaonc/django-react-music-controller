641614e654d747d9e272a1f65c990485964d015a
Joao Coelho <joaonc@hotmail.com>
2022-12-12 14:30:49 -0600

    create room endpoint

--- music_controller/api/serializers.py
+++ music_controller/api/serializers.py
@@ -14,3 +14,17 @@ class RoomSerializer(serializers.ModelSerializer):
             'votes_to_skip',
             'create_at',
         )
+
+
+class CreateRoomSerializer(serializers.ModelSerializer):
+    """Payload to create a room on a POST request.
+
+    Note that ``host`` (as well as ``room`` and ``created_at``) is calculated at creation time
+    (with session info) and not part of the payload.
+    """
+    class Meta:
+        model = Room
+        fields = (
+            'guest_can_pause',
+            'votes_to_skip',
+        )
