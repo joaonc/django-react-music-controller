f10db2225937a350024eea8ba8459baf00d30b99
Joao Coelho <joaonc@hotmail.com>
2022-12-12 15:40:21 -0600

    create room page html

--- music_controller/frontend/src/components/CreateRoomPage.js
+++ music_controller/frontend/src/components/CreateRoomPage.js
@@ -23,7 +23,61 @@ const CreateRoomPage = () => {
                     Create a Room
                 </Typography>
             </Grid>
+            <Grid item xs={12} align='center'>
+                <FormControl component='fieldset'>
+                    <FormHelperText>
+                        <div align='center'>
+                            Guest Control of Playback State
+                        </div>
+                    </FormHelperText>
+                    <RadioGroup row defaultValue={true}>
+                        <FormControlLabel
+                            value={true}
+                            control={<Radio color='primary'/>}
+                            label='Play/Pause'
+                            labelPlacement='bottom'
+                        />
+                        <FormControlLabel
+                            value={false}
+                            control={<Radio color='secondary'/>}
+                            label='No Control'
+                            labelPlacement='bottom'
+                        />
+                    </RadioGroup>
+                </FormControl>
+            </Grid>
+            <Grid item xs={12} align='center'>
+                <FormControl>
+                    <TextField
+                        required={true}
+                        type='number'
+                        defaultValue={defaultVotes}
+                        inputProps={{
+                            min: 1,
+                            style: {
+                                textAlign: 'center',
+                            },
+                        }}
+                    />
+                    <FormHelperText>
+                        <div align='center'>
+                            Votes Required to Skip Song
+                        </div>
+                    </FormHelperText>
+                </FormControl>
+            </Grid>
+            <Grid item xs={12} align='center'>
+                <Button color='primary' variant='contained'>
+                    Create a Room
+                </Button>
+            </Grid>
+            <Grid item xs={12} align='center'>
+                <Button color='secondary' variant='contained' to='/' component={Link}>
+                    Back
+                </Button>
+            </Grid>
         </Grid>
-    )}
+    )
+}
 
 export default CreateRoomPage
