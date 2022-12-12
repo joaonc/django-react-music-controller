a93633dd6a15c91954ac847f47c752a467aaaa1f
Joao Coelho <joaonc@hotmail.com>
2022-12-12 15:01:36 -0600

    material ui imports and initial use

--- music_controller/frontend/src/components/CreateRoomPage.js
+++ music_controller/frontend/src/components/CreateRoomPage.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import {
+    Button,
+    FormControl,
+    FormControlLabel,
+    FormHelperText,
+    Grid,
+    Radio,
+    RadioGroup,
+    TextField,
+    Typography
+} from '@mui/material'
+
 
 const CreateRoomPage = () => {
+    const defaultVotes = 2
+
     return (
-        <p>This is the create room page</p>
-    )
-}
+        <Grid container spacing={1}>
+            <Grid item xs={12} align='center'>
+                <Typography component='h4' variant='h4'>
+                    Create a Room
+                </Typography>
+            </Grid>
+        </Grid>
+    )}
 
 export default CreateRoomPage
